fa531b24446a6db0813c088f64ba9b272ac2ee21
danielfranchi <danielfranchi3@gmail.com>
2023-05-11 23:19:44 -0300

    commit

--- src/app/GameThree/page.jsx
+++ src/app/GameThree/page.jsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import Icon from "./icon";
+import Icon from "./Icon";
 
 const page = () => {
   const [turn, setTurn] = useState("x");
